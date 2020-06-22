29f98466f90685328192ec6b8bca85041d263e30
Count Wilhelm <count@debianmini.madscott.org>
2020-06-21 22:04:08 -0700

    db again

--- rapidpythonweb/settings.py
+++ rapidpythonweb/settings.py
@@ -91,23 +91,23 @@ WSGI_APPLICATION = 'rapidpythonweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql_psycopg2',
-       'NAME': 'quickpython',
-       'USER': 'quickpython',
-       'PASSWORD': 'dune02',
-       'HOST': 'localhost',
-       'PORT': '5432',
-   }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+#
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'quickpython',
+#        'USER': 'quickpython',
+#        'PASSWORD': 'dune02',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#    }
+# }
 
 DATABASES = {'default': dj_database_url.config()}
 
